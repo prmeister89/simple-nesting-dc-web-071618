b8ee0b76ec3b6060678b5f3f6a01a748a6370ed0
Peter Meister <prmeister89@gmail.com>
2018-07-10 19:29:45 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+  programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:dennis_ritchie][:languages][0]
+  
 end
 
 def adding_matz
@@ -79,8 +85,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  
+  programmer_hash[:yukihiro_matsumoto] = {:yukihiro_matsumoto => {
+    :known_for => "Ruby",:languages => ["LISP", "C"]}}
+  
 end
 
 def changing_alan
@@ -102,6 +110,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  return programmer_hash
      
 end
 
@@ -121,5 +131,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+  return programmer_hash
 
 end
