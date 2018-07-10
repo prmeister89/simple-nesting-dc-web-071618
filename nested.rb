c5fefca1af709b783e356dd7b350c30e2ec41888
Peter Meister <prmeister89@gmail.com>
2018-07-10 18:32:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,8 +85,13 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  
+  programmer_hash
+    :yukihiro_matsumoto => {
+      :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+  
 end
 
 def changing_alan
